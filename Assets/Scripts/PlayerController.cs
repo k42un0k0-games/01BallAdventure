63e8cc503afae3a44fbb7d29b813028cc0ce4ca5
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-04-07 08:45:47 +0900

    add ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * 5 * verticalInput);
+        transform.Rotate(Vector3.up, 20.0f * Time.deltaTime * horizontalInput);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("keydown space");
+            rb.AddForce(1, 10.0f, 0, ForceMode.Impulse);
+        }
+    }
+}
